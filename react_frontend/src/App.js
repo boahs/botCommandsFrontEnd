820912b7e25982e79d137d638eb95d8425807477
janis korisv <nooby4life19@gmail.com>
2022-01-02 16:27:31 -0500

    finished

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo193.png'
+
 
 function App() {
   return (
@@ -7,16 +8,59 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>BoahsBot</code> master command list
         </p>
+        <section className = "SMALLSECONDHEADER">
+          Click triangle for dropdown of all commands
+        </section>
+        <section className = "LIST">
+          <details>
+        <summary>
+        Commands
+        </summary>
+        <li>!commands</li>
+        <li>!pan</li>
+        <li>!jungle</li>
+        <li>!shining</li>
+        <li>!xmen</li>
+        <li>!crueball</li>
+        <li>!bio</li>
+        <li>!nhl</li>
+        <li>!chakan</li>
+        <li>!carnage</li>
+        <li>!champions</li>
+        <li>!sonic</li>
+        <li>!light</li>
+        <li>!mercs</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!game1</li>
+        <li>!voyage</li>
+        <li>!dirtydozen</li>
+        <li>!ggtv</li>
+        <li>!2021Dozen</li>
+        <li>!deals</li>
+
+          </details>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitch.tv/boahs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow Boahs
         </a>
+        </section>
       </header>
     </div>
   );
