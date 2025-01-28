c79eb9af8095652948f59eeed5266440bf89a5d0
Sha Adkins <sha.adkins@infor.com>
2025-01-27 21:35:23 -0500

    d

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -36,7 +36,9 @@ function App() {
       <header className="App-header">
         <h2>Countdown to Next Dirty Dozen: {countdown}</h2>
         <h2>Bot created for The Dirty Dozen</h2>
+        <section className="botStatus">
         <h2>Bot status: Online</h2>
+        </section>
         <code>
           <img src={githubIcon} alt="GitHub icon" className="icon" />
           <a 
@@ -62,7 +64,7 @@ function App() {
             <p>!dirtydozen</p>
             <p>!ggtv</p>
             <p>!deals</p>
-            <h3>Dirty Dozen 2022</h3>
+            <h3>Dirty Dozen 2025</h3>
             <p>TBA!</p>
             <h3>Dirty Dozen 2024</h3>
             <p>!psychoPinball</p>
