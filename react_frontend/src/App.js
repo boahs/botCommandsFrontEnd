c889e916c689f5b17d3da30e77eeceb25dc4f498
Sha Adkins <sha.adkins@infor.com>
2025-02-16 15:25:12 -0500

    .

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -75,7 +75,7 @@ function App() {
         <h2>Bot created for The Dirty Dozen</h2>
         <section className="botStatus">
           <p>
-            Bot Status: <span className="onlineText">OFFLINE</span>
+            Bot Status: <span className="onlineText">ONLINE</span>
           </p>
         </section>
 
